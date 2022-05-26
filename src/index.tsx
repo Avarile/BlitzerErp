f33cf3ce4377ef691557633a14c9aeeb4306cea1
Avarile <avarile@gmail.com>
2022-05-26 12:17:55 +1000

    load resources by https instead of http

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename="/dashboard">
+      <Router basename="/blitzererp">
         <App />
       </Router>
     </Provider>
